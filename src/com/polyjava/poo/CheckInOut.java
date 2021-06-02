cf91c4c2b3a62578dfa7f6a61443128948b908eb
jean augier <jean.augier@etu.univ-tours.fr>
2021-06-02 14:56:25 +0200

    debut javadoc

--- src/com/polyjava/poo/CheckInOut.java
+++ src/com/polyjava/poo/CheckInOut.java
@@ -1,24 +1,51 @@
 package com.polyjava.poo;
 
 import java.io.Serializable;
-
+/**
+ * Class checkInout which "save" when an employee come in the company and when he goes out 
+ * 
+ *
+ */
 public class CheckInOut extends Time implements Serializable{
 	
+	/**
+	 * Creation of an employee object when user click on check in/out
+	 * <p>
+	 * @see CheckInOut(Employee employeeCheck)
+	 * @see Employee getEmployeeCheck()
+	 * @see setEmployeeCheck(Employee employeeCheck)
+	 */
 	private Employee EmployeeCheck;
 
+	/**
+	 * Constructor of a Check in/out
+	 * <p>
+	 * @param employeeCheck
+	 */
 	public CheckInOut(Employee employeeCheck) {
 		super();
 		EmployeeCheck = employeeCheck;
 	}
 
+	/**
+	 * Constructor
+	 */
 	public CheckInOut() {
 		super();
 	}
 
+	/**
+	 * Getter
+	 * @return EmployeeCheck
+	 */
 	public Employee getEmployeeCheck() {
 		return EmployeeCheck;
 	}
 
+	/**
+	 * Setter
+	 * @param employeeCheck
+	 */
 	public void setEmployeeCheck(Employee employeeCheck) {
 		EmployeeCheck = employeeCheck;
 	}
--- src/com/polyjava/poo/Company.java
+++ src/com/polyjava/poo/Company.java
@@ -2,39 +2,93 @@ package com.polyjava.poo;
 
 
 import java.util.ArrayList;
-
+/**
+ * Class Company which manage the department of the company 
+ *
+ */
 public class Company {
 
+	/**
+	 * Name of the company 
+	 * <p>
+	 * @see Company(String nameCompany, ArrayList<Department> departmentList)
+	 * @see getNameCompany()
+	 * @see setNameCompany(String nameCompany)
+	 */
 	private String nameCompany;
 
+	/**
+	 * List of the department
+	 * <p>
+	 * @see Company(String nameCompany, ArrayList<Department> departmentList)
+	 * @see ArrayList<Department>{@link #getDepartmentList()}
+	 * @see setDepartmentList(ArrayList<Department> departmentList)
+	 * @see addDepartement(Department newDpt)
+	 * @see removeDepartement(Department dpt)
+	 */
 	private ArrayList<Department> DepartmentList;
 
-	
+	/**
+	 * Constructor of the class Company
+	 * <p>
+	 * @param nameCompany
+	 * @param departmentList
+	 */
 	public Company(String nameCompany, ArrayList<Department> departmentList) {
 		this.nameCompany = nameCompany;
 		DepartmentList = departmentList;
 	}
 	
+	/**
+	 * Getter of nameCompany
+	 * <p>
+	 * @return nameCompany
+	 */
 	public String getNameCompany() {
 		return nameCompany;
 	}
 
+	/**
+	 * Setter of nameCompany
+	 * <p>
+	 * @param nameCompany
+	 */
 	public void setNameCompany(String nameCompany) {
 		this.nameCompany = nameCompany;
 	}
 
+	/**
+	 * Getter of the department list
+	 * <p>
+	 * @return DepartmentList
+	 */
 	public ArrayList<Department> getDepartmentList() {
 		return DepartmentList;
 	}
 
+	/**
+	 * Setter of the department list
+	 * <p>
+	 * @param departmentList
+	 */
 	public void setDepartmentList(ArrayList<Department> departmentList) {
 		DepartmentList = departmentList;
 	}
 	
+	/**
+	 * Method which add a department to the DepartmentList
+	 * <p>
+	 * @param newDpt
+	 */
 	public void addDepartement(Department newDpt) {
 		this.DepartmentList.add(newDpt);
 	}
 	
+	/**
+	 * Method which delete a department from the DepartmentList
+	 * <p>
+	 * @param dpt
+	 */
 	public void removeDepartement(Department dpt) {
 		this.DepartmentList.remove(dpt);
 	}
